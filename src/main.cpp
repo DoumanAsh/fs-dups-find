722ce077ddd77baae90e4a15b0f5801d7c1146b7
Douman <douman@gmx.se>
2018-05-13 13:25:20 +0300

    Check dirs are part of one another

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 
 namespace fs = std::filesystem;
 
-void get_files_digest(std::map<std::string, std::vector<std::string>>& file_hashes, fs::recursive_directory_iterator dir_it) {
+static void get_files_digest(std::map<std::string, std::vector<std::string>>& file_hashes, fs::recursive_directory_iterator dir_it) {
     Hasher hasher;
     for (auto&& entry: dir_it) {
         if (!entry.is_symlink() && entry.is_regular_file()) {
@@ -28,38 +28,56 @@ void get_files_digest(std::map<std::string, std::vector<std::string>>& file_hash
     }
 }
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
     if (argc < 3) {
         fmt::print("Usage: fs-dips-find <path1> <path2>\n");
         return 1;
     }
 
-    std::error_code error_code;
-    const fs::path first(argv[1]);
-    fs::recursive_directory_iterator first_it(first, fs::directory_options::none, error_code);
+    std::vector<fs::recursive_directory_iterator> dirs;
+    {
+        std::error_code error_code;
+        const fs::path first = fs::absolute(argv[1]);
+        const fs::path second = fs::absolute(argv[2]);
+        fs::recursive_directory_iterator first_it(first, fs::directory_options::none, error_code);
 
-    if (error_code) {
-        fmt::print("{}: Error opening: {}\n", first.generic_string(), error_code.message());
-        return 1;
-    }
+        if (error_code) {
+            fmt::print("{}: Error opening: {}", argv[1], error_code.message());
+            return 1;
+        }
 
-    const fs::path second(argv[2]);
-    fs::recursive_directory_iterator second_it(second, fs::directory_options::none, error_code);
+        fs::recursive_directory_iterator second_it(second, fs::directory_options::none, error_code);
 
-    if (error_code) {
-        fmt::print("{}: Error opening: {}\n", second.generic_string(), error_code.message());
-        return 1;
+        if (error_code) {
+            fmt::print("{}: Error opening: {}", argv[2], error_code.message());
+            return 1;
+        }
+
+        //We do not walk a directory if it is already inside another one
+        //Simple comparison using abs path should give us it... hopefully
+        if (first.native().find(second.native()) == fs::path::string_type::npos) {
+            dirs.emplace_back(std::move(first_it));
+        }
+        if (second.native().find(first.native()) == fs::path::string_type::npos) {
+            dirs.emplace_back(std::move(second_it));
+        }
+
+        if (dirs.empty()) {
+            //Well if we're supplied with the same paths
+            //then at least try to look for dups in one
+            dirs.emplace_back(std::move(first_it));
+        }
     }
 
     std::map<std::string, std::vector<std::string>> file_hashes;
-    get_files_digest(file_hashes, first_it);
-    get_files_digest(file_hashes, second_it);
+    for (auto&& dir : dirs) {
+        get_files_digest(file_hashes, dir);
+    }
 
-    for (const auto& [digest, files] : file_hashes) {
+    for (auto&& [digest, files] : file_hashes) {
         if (files.size() > 1) {
             fmt::print("Found duplicate files with hash {}:\n", digest);
-            for (const auto& file : files) {
+            for (auto&& file : files) {
                 fmt::print("{}\n", file);
             }
 
